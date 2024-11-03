df6e4486a3323cef8479e7faecd7a0bd521d6cf3
lagosyusuf <lagosyusuf@gmail.com>
2024-11-02 19:44:27 -0700

    First commit for grading system implementation.

---
+++ grading-system.py
@@ -0,0 +1,11 @@
+gradeMarks = int(input("Enter your Grade: "))
+if gradeMarks >= 90:
+    print("Grade - Excellent")
+elif gradeMarks < 90 and gradeMarks >= 75:
+    print("Grade - First Class")
+elif gradeMarks < 75 and gradeMarks >= 40:
+    print("Grade - Average")
+else :
+    print("Grade - Fail")
+    # comment: 
+            
